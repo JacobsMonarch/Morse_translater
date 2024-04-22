baff4b20d7605a23fdeb8d1bbe2b44c129c5ddd2
JacobsMonarch <54481450+JacobsMonarch@users.noreply.github.com>
2024-04-22 23:30:47 +0300

    GUI Morse_Translator
    Added a basic graphical interface for the program. It looks disgusting

--- Morse_Translator.py
+++ Morse_Translator.py
@@ -1,4 +1,20 @@
-#Словарь с азбукой Морзе
+import sys
+
+from PySide6.QtCore import QSize, Qt, QRect
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout,
+)
+
+def translating(text):
+    code_morse = ""
+    for char in text:
+        if char.upper() in Morse:
+            code_morse += Morse[char.upper()] + " "
+        else:
+            code_morse += char + " "
+    return code_morse
+
+# Словарь с азбукой Морзе
 Morse = {"А":"*-","Б":"-***","В":"*--","Г":"--*","Д":"-**",
 "Е":"*","Ж":"***-","З":"--**","И":"**",
 "Й":"*---","К":"-*-","Л":"*-**","М":"--","Н":"-*",
@@ -7,22 +23,75 @@ Morse = {"А":"*-","Б":"-***","В":"*--","Г":"--*","Д":"-**",
 "Ш":"----","Щ":"--*-","Ъ":"*--*-*","Ы":"-*--","Ь":"-**-",
 "Э":"**-**","Ю":"**--","Я":"*-*-"}
 
-#Функция
-def translating(text):
-#После каждой итерации в данную функцию будет добавляться символ
-  code_morse = ''
-  for char in text:
-    if char.upper() in Morse:
-      code_morse += Morse[char.upper()] + ' '
-    else:
-      code_morse += char + ' '
-  return code_morse
+class MorseTranslator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Переводчик на азбуку Морзе")
 
-# Ввод текста пользователем
-text = input("Введите текст на русском языке: ")
+        #Создание окна
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.resize(350, 350)
+        self.setMinimumSize(QSize(350, 350))
+        self.setMaximumSize(QSize(350, 350))
+        self.setStyleSheet(u"background-color: rgb(151, 154, 170);")
 
-# Перевод текста на азбуку Морзе
-code_morse = translating(text)
+        self.logo_label = QLabel("Morse code Translator")
+        self.logo_label.setGeometry(QRect(0, 1, 350, 20)) #Почему не работает??
+        self.logo_label.setMinimumSize(QSize(350, 20))
+        self.logo_label.setMaximumSize(QSize(350, 20))
+        self.logo_label.setAlignment(Qt.AlignCenter)
+        self.logo_label.setStyleSheet(u'font: 800 16pt "Montserrat";')
+        self.layout.addWidget(self.logo_label)
 
-# Вывод результата
-print("Текст в азбуке Морзе:", code_morse)
+        #Текст перед вводом
+        self.input_label = QLabel("Введите текст на русском языке:")
+        self.input_label.setGeometry(QRect(-5, 100, 350, 20)) #Почему не работает??
+        self.input_label.setMinimumSize(QSize(350,20))
+        self.input_label.setMaximumSize(QSize(350,20))
+        self.input_label.setAlignment(Qt.AlignCenter)
+        self.input_label.setStyleSheet(u'font: italic 12pt "Montserrat";')
+        self.layout.addWidget(self.input_label)
+
+        #Поле для ввода
+        self.input_field = QLineEdit()
+        self.input_field.setMinimumSize(QSize(325, 35))
+        self.input_field.setMaximumSize(QSize(325, 35))
+        self.input_field.setClearButtonEnabled(True)
+        self.layout.addWidget(self.input_field)
+
+        #Кнопка "Перевести"
+        self.translate_button = QPushButton("Перевести")
+        self.translate_button.setMinimumSize(QSize(325, 35))
+        self.translate_button.setMaximumSize(QSize(325, 35))
+        self.translate_button.clicked.connect(self.translate_text)
+        self.layout.addWidget(self.translate_button)
+
+        #Текст перед выводом
+        self.output_label = QLabel("Текст в азбуке Морзе:")
+        self.output_label.setGeometry(QRect(-5, 100, 350, 20)) #Почему не работает??
+        self.output_label.setMinimumSize(QSize(350,20))
+        self.output_label.setMaximumSize(QSize(350,20))
+        self.output_label.setAlignment(Qt.AlignCenter)
+        self.output_label.setStyleSheet(u'font: italic 12pt "Montserrat";')
+        self.layout.addWidget(self.output_label)
+
+        #Поле вывода
+        self.output_field = QLineEdit()
+        self.output_field.setReadOnly(True)
+        self.output_field.setMinimumSize(QSize(325, 35))
+        self.output_field.setMaximumSize(QSize(325, 35))
+        self.layout.addWidget(self.output_field)
+
+    def translate_text(self):
+        input_text = self.input_field.text().strip()
+        self.output_field.setText("")
+        if input_text:
+            morse_code = translating(input_text)
+            self.output_field.setText(morse_code)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    translator = MorseTranslator()
+    translator.show()
+    sys.exit(app.exec())
